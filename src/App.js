d69db111254256367c5f271c072d3d2be0609ff2
함창수 <ham@hamchangsuui-MacBook-Pro.local>
2021-01-03 16:27:52 +0900

    ~#5.0 life cycle, styling, deploy

--- src/App.js
+++ src/App.js
@@ -1,54 +1,48 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from 'axios';
+import Movie from './components/Movie';
+import "./App.css"
 
-function Food({ name, rating, picture }) {
-	return (
-    <div>
-      <h3>내가 좋아하는 음식: {name}</h3>
-      <h4>선호도: {rating} / 10</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-  picture: PropTypes.string.isRequired
-}
-
-const foodILike = [
-  {
-    id: 0,
-    name: "김치",
-    rating: 3,
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IDrNtl4pG_zepTDko3IUhgHaHa%26pid%3DApi&f=1"
-  },
-  {
-    id: 1,
-    name: "짜장",
-    rating: 2.5,
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DxzM698upNhEDSm6_A5agQHaE6%26pid%3DApi&f=1"
-  },
-  {
-    id: 2,
-    name: "밥",
-    rating: 6.1,
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpds.joins.com%2Fnews%2FFbMetaImage%2F201811%2F2f2968c7-c62f-4358-bae3-e59121293e05.jpg&f=1&nofb=1"
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   }
-]
 
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} rating={dish.rating} picture={dish.image} />
-}
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies, isLoading: false});
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello Ham</h1>
-      {foodILike.map(renderFood)}
-    </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading, movies} =  this.state;
+    return (
+      <section className="container">
+          {isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div>
+            ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres} />
+              ))}
+            </div>
+          )}
+      </section>
+    )
+  }
 }
 
 export default App;
